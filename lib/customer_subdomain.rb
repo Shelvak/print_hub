19990b139fac290a9964122cc028cc8bef3bc783
Néstor Coppi <nestorcoppi@gmail.com>
2013-03-11 20:22:57 -0300

    Customer host =/

--- lib/customer_subdomain.rb
+++ lib/customer_subdomain.rb
@@ -1,5 +1,6 @@
 class CustomerSubdomain
   def self.matches?(request)
-    request.host =~ APP_CONFIG['public_host']
+    request.host == APP_CONFIG['public_host'] ||
+      request.host == "www.#{APP_CONFIG['public_host']}"
   end 
 end
